9bc9bdc21ed098d5a2da6c8e3d3d9f43dbebbf3f
Paul Wolfgang <wolfgang@temple.edu>
2018-05-25 12:06:43 -0400

    Added LICENCE and README.mc

--- src/main/java/edu/temple/cla/papolicy/wolfgang/texttools/util/Util.java
+++ src/main/java/edu/temple/cla/papolicy/wolfgang/texttools/util/Util.java
@@ -39,7 +39,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
