eb36263cb1f89c9b76436f71b4ab84454f37a7e2
Paul Wolfgang <wolfgang@temple.edu>
2018-06-10 18:57:54 -0400

    Added EvenOddFilter.

---
+++ src/main/java/edu/temple/cla/papolicy/wolfgang/texttools/util/EvenOddFilter.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2018, Temple University
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * All advertising materials features or use of this software must display 
+ *   the following  acknowledgement
+ *   This product includes software developed by Temple University
+ * * Neither the name of the copyright holder nor the names of its 
+ *   contributors may be used to endorse or promote products derived 
+ *   from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.temple.cla.papolicy.wolfgang.texttools.util;
+
+import java.util.function.Predicate;
+
+/**
+ *
+ * @author Paul
+ */
+public class EvenOddFilter implements Predicate<Object>{
+    
+    private int counter;
+    private boolean both;
+    private int remander;
+    public enum TYPE {BOTH, EVEN, ODD}
+    public EvenOddFilter(TYPE type) {
+        counter = 0;
+        switch (type) {
+            case BOTH: both=true;
+            break;
+            case EVEN:
+                both = false;
+                remander = 0;
+                break;
+            case ODD:
+                both = false;
+                remander = 1;
+                break;
+        }
+    }
+    
+    @Override
+    public boolean test(Object o) {
+        if (both) return true;
+        return (counter++ % 2 == remander);
+    }
+}
